8cdc024c8db168166ded5477eb3d2605d5006f30
EmyAssis75 <166054739+EmyAssis75@users.noreply.github.com>
2024-06-19 13:59:05 -0300

    index.js

--- src/rotas/index.js
+++ src/rotas/index.js
@@ -1,15 +1,1 @@
-import { createBottomTabNavigator } from '@react-navugation/bottom-tabs';
 
-const { Screen, Navigator } = createBottomTabNavigator();
-
-import { TelaA } from '../telas/tabelaA';
-import { TelaB } from '../telas/tabelaB';
-
-export function TabRotas(){
-  return(
-    <Navigator>
-      <Screen name='TelaA' component={TelaA} />
-      <Screen name='TelaB' component={TelaB} />
-    <Navigator/>
-  );
-};
