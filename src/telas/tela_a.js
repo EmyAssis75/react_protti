272e0cad8ec9828f37114f6de4d939461cdf892b
EmyAssis75 <166054739+EmyAssis75@users.noreply.github.com>
2024-06-19 13:37:32 -0300

    tela_a.js

--- src/telas/tela_a.js
+++ src/telas/tela_a.js
@@ -1,1 +1,21 @@
+import React from "react";
+import { StyleSheet, View, Text, FlatList } from "react-native";
 
+export function TelaA() {
+  const DADOS = [
+    { id: "1", title: "Primeiro Item" ,},
+    { id: "2", title: "Segundo Item" ,},
+    { id: "3", title: "Terceiro Item" ,},
+  ];
+
+  return (
+    <View style={styles.container}>
+     <FlatList data={DADOS} renderItem={({item, index, separators}) => (
+        <View style={styles.lista}>
+          <View>
+            <Text style={styles.texto}>{item.id} : {item.title}</Text>
+          </View>
+        </View>
+       )}>
+     </View>
+  );
